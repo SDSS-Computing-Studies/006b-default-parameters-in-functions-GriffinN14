654e695aa4f1555de9613ab598bc39a3695c2dbe
GriffinN14 <71406855+GriffinN14@users.noreply.github.com>
2020-10-09 12:21:19 -0700

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -2,7 +2,15 @@
 
 import math
 
-def tempConversion():
+def tempConversion(degrees, unit=("C")):
+    if unit == ("C"):
+        f = (degrees * 9 / 5) + 32
+        f = round(f,1)
+        return f
+    else:
+        c = (degrees - 32) * 5 / 9
+        c = round(c,1)
+        return c
 
 
 def factorPair():
