38c35cdeae336dc0a4a2e7b4c4817052fbe4b035
GriffinN14 <71406855+GriffinN14@users.noreply.github.com>
2020-10-09 12:37:05 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,9 +1,4 @@
 #!python3
 
-list = []
-def factorPair(a,b):
-    for i in range(a):
-        if i > 1:
-            if a / i == int(a / i):
-                list.append(i)
-    return list
+import assignment
+print (assignment.factorPair(10,2))
