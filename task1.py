5297aae51c7fce6d30a0f7131049944cc76135ed
GriffinN14 <71406855+GriffinN14@users.noreply.github.com>
2020-10-09 11:57:18 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,4 +1,12 @@
 #!python3
 
 import assignment
-
+def tempConversion(degrees, unit=("C")):
+    if unit == ("C"):
+        f = (degrees * 9 / 5) + 32
+        f = round(f,1)
+        return f
+    else:
+        c = (degrees - 32) * 5 / 9
+        c = round(c,1)
+        return c
